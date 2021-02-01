d6d962748826a740c4e216a71a8b9f0578126adb
EugeneSel <youdjin.sel15@gmail.com>
2021-02-02 00:21:56 +0100

    fine tuned transformer

--- dataloader.py
+++ dataloader.py
@@ -1,23 +1,28 @@
 from transformers import BertTokenizer
-import torch
+
 import pandas as pd
+
+import torch
 from torch.utils.data import TensorDataset, random_split
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 
-batch_size = 32
+batch_size = 64
+NUMBER_OF_SAMPLES = 250
 
 def tokenize_data(path, names, delimiter='\t'):
     #df = pd.read_csv(path, delimiter=delimiter, header=None, names=names)
-    df = pd.read_csv(path, delimiter=delimiter, header=None, names=names)[0:100]
-    #print(df.head())
+    df = pd.read_csv(path, delimiter=delimiter, header=None, names=names)
+    df = df.sample(frac=1)[:NUMBER_OF_SAMPLES]
+    # print(df.head())
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
     if "sentence" in names and "label" in names:
         sentences = df.sentence.values
         labels = df.label.values
     else :
         print("EXIT")
-        return None,None
+        return None, None
     num_labels = len(set(labels))
+    unique_labels = df.label.unique().tolist()
     max_len = 0
 
     # For every sentence...
@@ -46,15 +51,15 @@ def tokenize_data(path, names, delimiter='\t'):
         #   (6) Create attention masks for [PAD] tokens.
         encoded_dict = tokenizer.encode_plus(
                             sent,                      # Sentence to encode.
-                            add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                            max_length = max_len,  
+                            add_special_tokens=True, # Add '[CLS]' and '[SEP]'
+                            max_length=max_len,  
                             truncation=True,         
-                            pad_to_max_length = True,   # Pad & truncate all sentences.
-                            return_attention_mask = True,   # Construct attn. masks.
-                            return_tensors = 'pt',     # Return pytorch tensors.
+                            pad_to_max_length=True,   # Pad & truncate all sentences.
+                            return_attention_mask=True,   # Construct attn. masks.
+                            return_tensors='pt',     # Return pytorch tensors.
                     )
         
-        # Add the encoded sentence to the list.    
+        # Add the encoded sentence to the list.
         input_ids.append(encoded_dict['input_ids'])
         
         # And its attention mask (simply differentiates padding from non-padding).
@@ -68,10 +73,10 @@ def tokenize_data(path, names, delimiter='\t'):
     # Combine the training inputs into a TensorDataset.
     dataset = TensorDataset(input_ids, attention_masks, labels)
 
-    return dataset, num_labels
+    return df, dataset, num_labels, unique_labels
 
 
-def load_data(batch_size):
+def load_data(dataset, batch_size):
     # Create a 90-10 train-validation split.
 
     # Calculate the number of samples to include in each set.
@@ -85,31 +90,32 @@ def load_data(batch_size):
     print('{:>5,} validation samples'.format(val_size))
     train_dataloader = DataLoader(
             train_dataset,  # The training samples.
-            sampler = RandomSampler(train_dataset), # Select batches randomly
-            batch_size = batch_size # Trains with this batch size.
+            sampler=RandomSampler(train_dataset), # Select batches randomly
+            batch_size=batch_size # Trains with this batch size.
         )
 
     # For validation the order doesn't matter, so we'll just read them sequentially.
     validation_dataloader = DataLoader(
             val_dataset, # The validation samples.
-            sampler = SequentialSampler(val_dataset), # Pull out batches sequentially.
-            batch_size = batch_size # Evaluate with this batch size.
+            sampler=SequentialSampler(val_dataset), # Pull out batches sequentially.
+            batch_size=batch_size # Evaluate with this batch size.
         )
-    return train_dataloader,validation_dataloader
+    return train_dataloader, validation_dataloader
 
 
-# path = "./.data/yelp_review_full_csv/train.csv"
-# path = "cola_public/cola_public/raw/in_domain_train.tsv"
-# names=['label', 'sentence']
-# dataset,num_labels = tokenize_data(path, names, delimiter=',')
+if __name__ == "__main__":
+    # path = "./.data/yelp_review_full_csv/train.csv"
+    # path = "cola_public/cola_public/raw/in_domain_train.tsv"
+    # names=['label', 'sentence']
+    # dataset,num_labels = tokenize_data(path, names, delimiter=',')
 
-path = "./cola_public/cola_public/raw/in_domain_train.tsv"
-names = ['sentence_source', 'label', 'label_notes', 'sentence']
-dataset, num_labels = tokenize_data(path, names)
+    path = "./cola_public/cola_public/raw/in_domain_train.tsv"
+    names = ['sentence_source', 'label', 'label_notes', 'sentence']
+    df, dataset, num_labels, unique_labels = tokenize_data(path, names)
 
-train_loader, validation_loader = load_data(batch_size)
+    train_loader, validation_loader = load_data(dataset, batch_size)
 
-    
-print('number of labels = ', num_labels)
-print('Number of train batches = ', len(train_loader))
-print('Batch size =', batch_size)
+    print('number of labels = ', num_labels)
+    print(f"Unique labels: {unique_labels}")
+    print('Number of train batches = ', len(train_loader))
+    print('Batch size =', batch_size)
--- models/test_sequence_classification.py
+++ models/test_sequence_classification.py
@@ -1,14 +1,22 @@
 import torch
-from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
+from torch.nn import BCELoss, Softmax
+from torch.optim import Adam, lr_scheduler
+
+from transformers import AutoConfig, AutoModelForSequenceClassification, \
+                                get_linear_schedule_with_warmup
+
+from dataloader import tokenize_data, load_data
+
+from IPython.display import display
 
 
-def train(model, train_loader, scheduler, optimizer, loss_function, checkpoint=50):
+def train(model, train_loader, scheduler, optimizer, loss_function, checkpoint=1):
     model.train()
 
     avg_loss = 0
 
-    for index, batch in enumerate(1, train_loader):
-        ids, labels = batch[0], batch[1]
+    for index, batch in enumerate(train_loader, 1):
+        ids, mask, labels = batch[0], batch[1], batch[2]
         # x, y = x.to('cuda'), y.to('cuda')
         
         model.zero_grad() # re-init the gradients (otherwise they are cumulated)
@@ -16,6 +24,8 @@ def train(model, train_loader, scheduler, optimizer, loss_function, checkpoint=5
         # Forward pass: Compute predicted y by passing x to the model:
         outputs = model(
             ids,
+            # token_type_ids=None,
+            # attention_mask=mask, 
             labels=labels
         )
         
@@ -32,7 +42,7 @@ def train(model, train_loader, scheduler, optimizer, loss_function, checkpoint=5
         optimizer.step() # update the weights
     scheduler.step() # update the learning rate
 
-    avg_loss /= train_loader.shape[0]
+    avg_loss /= len(train_loader)
     
     return avg_loss
 
@@ -42,75 +52,124 @@ def evaluate(model, test_loader):
 
     avg_loss = 0
     avg_accuracy = 0
+    prediction_list = []
+
     for batch in test_loader:
-        ids, labels = batch[0], batch[1]
+        ids, mask, labels = batch[0], batch[1], batch[2]
 
         with torch.no_grad():
             outputs = model(
                 ids,
+                # token_type_ids=None,
+                # attention_mask=mask,
                 labels=labels
             )
         
         loss = outputs.loss
         logits = outputs.logits
-        predictions = torch.argmax(logits, dim=1).flatten()
+        softmax = Softmax(dim=1)
+        predictions = torch.argmax(softmax(logits), dim=1).flatten()
+        prediction_list.append(predictions)
+        print(softmax(logits))
 
         avg_loss += loss.item()
-        avg_accuracy += (predictions == logits).sum() / predictions.shape[0]
+        avg_accuracy += (predictions == labels).sum() / len(predictions)
 
-    avg_loss /= test_loader.shape[0]
-    avg_accuracy /= test_loader.shape[0]
+    avg_loss /= len(test_loader)
+    avg_accuracy /= len(test_loader)
 
-    return avg_loss, avg_accuracy
+    return avg_loss, avg_accuracy, torch.cat(prediction_list, 0)
 
 
 # Use BERT model as an example:
 TRANSFORMER = 'bert-base-uncased'
+EPOCHS = 15
+BATCH_SIZE = 32
+LEARNING_RATE = .01
 
 
 if __name__ == "__main__":
-    test_categories = ["clothes", "home", "auto"]
-    test_strs = [
-        "This T-shirt is red",
-        "We've got some fridge in here",
-        "An iconic Ferrari wheel"
-    ]
-    max_seq_len = 10
+    # test_categories = ["clothes", "home", "auto"]
+    # test_strs = [
+    #     "This T-shirt is red",
+    #     "We've got some fridge in here",
+    #     "An iconic Ferrari wheel"
+    # ]
+    # max_seq_len = 10
 
     # Load tokenizer:
-    tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER)
+    # tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER)
+    # Download model from huggingface.co:
+    # model = AutoModelForSequenceClassification.from_pretrained(
+    #     TRANSFORMER,
+    #     num_labels=len(test_categories)
+    # )
+    # print(model.config.output_attentions)
+
+    # inputs = [
+    #     tokenizer(
+    #         test_str,
+    #         max_length=max_seq_len,
+    #         pad_to_max_length=True,
+    #         return_tensors="pt"
+    #     ) for test_str in test_strs]
+    # input_ids = torch.cat([input_["input_ids"] for input_ in inputs], dim=0)
+
+    # model.eval()
+    # labels = torch.tensor([0, 1, 2]).unsqueeze(0)  # Batch size 3
+    # with torch.no_grad():
+    #     outputs = model(
+    #         input_ids,
+    #         labels=labels
+    #     )
+        
+    #     loss = outputs.loss
+    #     logits = outputs.logits
+    #     predictions = torch.argmax(logits, dim=1).flatten()
+
+    # print(predictions)
+    # print(loss)
+    # print(logits)
+
+    # for pred in predictions:
+    #     predicted_token = test_categories[pred]
+    #     print(pred, predicted_token)
+
+    # Load and transform data:
+    path = "./cola_public/cola_public/raw/in_domain_train.tsv"
+    names = ['sentence_source', 'label', 'label_notes', 'sentence']
+
+    print("Loading data...")
+    df, dataset, num_labels, unique_labels = tokenize_data(path, names)
+    train_loader, validation_loader = load_data(dataset, BATCH_SIZE)
+
     # Download model from huggingface.co:
     model = AutoModelForSequenceClassification.from_pretrained(
         TRANSFORMER,
-        num_labels=len(test_categories)
+        output_attentions=True,
+        num_labels=num_labels
     )
-    # print(model.config.output_attentions)
 
-    inputs = [
-        tokenizer(
-            test_str,
-            max_length=max_seq_len,
-            pad_to_max_length=True,
-            return_tensors="pt"
-        ) for test_str in test_strs]
-    input_ids = torch.cat([input_["input_ids"] for input_ in inputs], dim=0)
+    # Define optimizer, loss and scheduler:
+    loss_function = BCELoss()
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=0,
+        num_training_steps=EPOCHS * len(train_loader)
+    )
 
-    model.eval()
-    labels = torch.tensor([0, 1, 2]).unsqueeze(0)  # Batch size 3
-    with torch.no_grad():
-        outputs = model(
-            input_ids,
-            labels=labels
-        )
-        
-        loss = outputs.loss
-        logits = outputs.logits
-        predictions = torch.argmax(logits, dim=1).flatten()
+    # train:
+    print("\nTraining:")
+    for epoch in range(1, EPOCHS + 1):
+        avg_loss = train(model, train_loader, scheduler, optimizer, loss_function)
+        print(f"Epoch {epoch}: cross entropy: {avg_loss}\n")
+    
+    # evaluate:
+    avg_loss, avg_accuracy, predictions = evaluate(model, validation_loader)
 
+    print("\nValidation:\n"
+          f"Val cross entropy: {avg_loss}; Val accuracy: {avg_accuracy}")
+
+    predicted_labels = [unique_labels[idx] for idx in predictions]
     print(predictions)
-    print(loss)
-    print(logits)
-
-    for pred in predictions:
-        predicted_token = test_categories[pred]
-        print(pred, predicted_token)
