037f917f774ae44fab408331b41b9f314da2389a
EugeneSel <youdjin.sel15@gmail.com>
2021-02-17 15:19:43 +0100

    refactored v0

--- models/test_sequence_classification_v0.py
+++ models/test_sequence_classification_v0.py
@@ -15,7 +15,7 @@ def train(model, train_loader):
         with torch.no_grad():
             batch_outputs = model(
                 ids,
-                token_type_ids=None,
+                # token_type_ids=None,
                 attention_mask=mask
             )
         outputs.append(batch_outputs[-1])
@@ -23,7 +23,7 @@ def train(model, train_loader):
     return torch.cat(outputs, 0)
 
 
-def evaluate(label_embeddings, labels, sample_embeddings, sample_labels):
+def evaluate(label_embeddings, labels, sample_embeddings, sample_labels, mean=False):
     cos = CosineSimilarity(dim=1)
 
     similarities = []
@@ -44,19 +44,27 @@ def evaluate(label_embeddings, labels, sample_embeddings, sample_labels):
 
 
 # Constants:
-MAX_LEN = None
+MAX_LEN = 20
 # NUMBER_OF_SAMPLES = 100
 # DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 DEVICE = torch.device("cpu")
 
 # Transformers to test:
-TRANSFORMER = [
-    'bert-base-uncased',
-    'google/mobilebert-uncased',
-    'funnel-transformer/small-base',
-    'roberta-base',
-    'facebook/bart-base', # bulky
-][0]
+TRANSFORMERS = {
+    "BERT": ('bert-base-uncased', False),
+    "MobileBERT": ('google/mobilebert-uncased', False),
+    "FunnelTransformer": ('funnel-transformer/small-base', True),
+    "RoBERTa": ('roberta-base', False),
+    "BART": ('facebook/bart-base', True), # bulky
+}
+
+# Datasets to test:
+DATASETS = [
+    "data_en.csv",
+    "data_en_with_parent.csv",
+    "data.csv",
+    "data_with_parent.csv"
+]
 
 # Hyperparameters:
 BATCH_SIZE = 1
@@ -66,55 +74,84 @@ LEARNING_RATE = 1E-4
 if __name__ == "__main__":
     import os
     import pandas as pd
+    from matplotlib import pyplot as plt
     from transformers import AutoModel, AdamW
     
     from dataloader import tokenize_data, load_data
     
     working_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-    # Load data:
-    print("Loading data...")
-    df = pd.read_csv(working_directory + "/sources/data_with_parent.csv", index_col=0)
-    df.category = df.category.apply(lambda x: x.replace(" >", ""))
-    df.category = df.category.astype("category")
-    print(df)
+    max_lengths = list(range(5, 60, 5)) + [None]
 
-    print("Transforming data...")
-    category_dataset, num_categories, unique_categories = tokenize_data(
-        df.category.values, df.category,
-        max_len=MAX_LEN, tokenizer=TRANSFORMER
-    )
+    # Make tests, plot results to compare:
+    for transformer_name, (transformer, mean) in TRANSFORMERS.items():
+        plt.figure(figsize=(15, 10))
+        for dataset in DATASETS:
+            # Load data:
+            print("Loading data...")
+            df = pd.read_csv(working_directory + "/sources/" + dataset, index_col=0)
+            df.category = df.category.apply(lambda x: x.replace(" >", ""))
+            df.category = df.category.astype("category")
+            print(df)
 
-    description_dataset, num_categories, unique_categories = tokenize_data(
-        df.description.values, df.category,
-        max_len=MAX_LEN, tokenizer=TRANSFORMER
-    )
+            accuracies = []
+            for max_len in max_lengths:
+                df_current = df.copy()
 
-    train_cat_loader = load_data(category_dataset, BATCH_SIZE, val=False)
-    test_description_loader = load_data(description_dataset, BATCH_SIZE, val=False)
+                print("Transforming data...")
+                category_dataset, num_categories, unique_categories = tokenize_data(
+                    df_current.category.values, df_current.category,
+                    max_len=None, tokenizer=transformer
+                )
 
-    # Download model from huggingface.co:
-    model = AutoModel.from_pretrained(
-        TRANSFORMER,
-        num_labels=num_categories,
-        output_attentions=False,
-        output_hidden_states=False
-    )
-    model.to(DEVICE)
+                description_dataset, num_categories, unique_categories = tokenize_data(
+                    df_current.description.values, df_current.category,
+                    max_len=max_len, tokenizer=transformer
+                )
 
-    cat_embeddings = train(model, train_cat_loader)
+                train_cat_loader = load_data(category_dataset, BATCH_SIZE, val=False)
+                test_description_loader = load_data(description_dataset, BATCH_SIZE, val=False)
 
-    description_embeddings = train(model, test_description_loader)
+                # Download model from huggingface.co:
+                model = AutoModel.from_pretrained(
+                    transformer,
+                    num_labels=num_categories,
+                    output_attentions=False,
+                    output_hidden_states=False
+                )
+                model.to(DEVICE)
 
-    predictions, predicted_categories, accuracy = evaluate(
-        cat_embeddings, df.category.cat.codes.unique().tolist(),
-        description_embeddings, description_dataset.tensors[2]
-    )
+                cat_embeddings = train(model, train_cat_loader)
+                description_embeddings = train(model, test_description_loader)
 
-    # Output results:
-    print()
-    print(f"Predicted category indices: {predicted_categories}")
-    print(f"Accuracy: {accuracy:.5f}")
+                # For Funnel and BART:
+                if mean:
+                    cat_embeddings = cat_embeddings.mean(dim=1).squeeze()
+                    description_embeddings = description_embeddings.mean(dim=1).squeeze()
 
-    df["predictions"] = [unique_categories[index] for index in predictions.numpy()]
-    print(df)
+                predictions, predicted_categories, accuracy = evaluate(
+                    cat_embeddings, df_current.category.cat.codes.unique().tolist(),
+                    description_embeddings, description_dataset.tensors[2]
+                )
+
+                accuracies.append(accuracy)
+
+                # Output results:
+                print()
+                print(f"Predicted category indices: {predicted_categories}")
+                print(f"Accuracy: {accuracy:.5f}")
+
+                df_current["predictions"] = [unique_categories[index] for index in predictions.numpy()]
+                print(df_current)
+
+            # Plot results:
+            plt.plot(max_lengths[:-1] + [60], accuracies)
+        plt.xlim(5, 60)
+        plt.xticks(ticks=max_lengths[:-1] + [60], labels=max_lengths[:-1] + ["None"])
+        plt.xlabel("Sequence length", fontsize=15)
+        plt.ylabel("Accuracy", fontsize=15)
+        plt.legend([ds.split(".")[0] for ds in DATASETS])
+        plt.title(f"{transformer_name}'s similarity task (v0) performance", fontsize=20)
+        if os.path.exists(working_directory + f"/images"):
+            plt.savefig(working_directory + f"/images/{transformer_name}_similarity_v0.png", dpi=300)
+        plt.show()
