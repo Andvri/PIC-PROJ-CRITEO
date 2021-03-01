6df042c7fc12ca10fd93d0ed68d141712e41ab6d
EugeneSel <youdjin.sel15@gmail.com>
2021-03-01 15:57:49 +0100

    Commented code, refactored

--- models/test_sequence_classification_v0.py
+++ models/test_sequence_classification_v0.py
@@ -6,6 +6,13 @@ from time import time
 
 
 def train(model, train_loader):
+    """
+    Compute word embeddings for the input sequences using given pre-trained transformer
+
+    TODO: add actual training pipeline with the resulting mean embeddings for all the categories
+    TODO: add cross-validation with weighted accuracy and the loss (cross-entropy)
+    """
+
     model.eval()
 
     outputs = []
@@ -24,20 +31,30 @@ def train(model, train_loader):
 
 
 def evaluate(label_embeddings, labels, sample_embeddings, sample_labels, mean=False):
+    """
+    Compute the cosine similarities with all the label_embeddings
+    for each sequence embedding vector from sample_embeddings
+    and take the argmax to predict sequence's category
+    """
     cos = CosineSimilarity(dim=1)
 
     similarities = []
+    # For every sequence vector in sample_embeddings:    
     for sample in sample_embeddings:
+        # Compute the similarities with all the label_embeddings:
         sample_sims = cos(
             torch.vstack([sample for _ in range(label_embeddings.shape[0])]), # explode given tensor
             label_embeddings
         )
         similarities.append(sample_sims)
+    # Stack similarities as one tensor:
     similarities = torch.vstack(similarities)
+    # Make argmax predictions:
     predictions = torch.argmax(similarities, dim=1).flatten()
 
     predicted_categories = torch.tensor([labels[index] for index in predictions])
 
+    # Compute simple (non-weighted) accuracy:
     accuracy = (predicted_categories == sample_labels).sum() / sample_embeddings.shape[0]
 
     return predictions, predicted_categories, accuracy
@@ -50,6 +67,9 @@ MAX_LEN = 20
 DEVICE = torch.device("cpu")
 
 # Transformers to test:
+# The second element of the tuple defines the type of transformer's output:
+# If False - transformer outputs only 1 embedding for each input sequence,
+# if True - transformer outputs embeddings for each sequence token, which will be aggregated:
 TRANSFORMERS = {
     "BERT": ('bert-base-uncased', False),
     "MobileBERT": ('google/mobilebert-uncased', False),
@@ -58,13 +78,8 @@ TRANSFORMERS = {
     "BART": ('facebook/bart-base', True), # bulky
 }
 
-# Datasets to test:
-DATASETS = [
-    "data_en.csv",
-    "data_en_with_parent.csv",
-    "data.csv",
-    "data_with_parent.csv"
-]
+# Dataset to test:
+DATASET = "data_en_train_with_parent.csv"
 
 # Hyperparameters:
 BATCH_SIZE = 1
@@ -82,78 +97,79 @@ if __name__ == "__main__":
     
     working_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+    # Sequence lenghts to vary (careful with the long sequences): 
     max_lengths = list(range(5, 60, 5)) + [None]
 
     # Make tests, plot results to compare:
+    # First tests can be done for only one transformer:
     for transformer_name, (transformer, mean) in TRANSFORMERS.items():
         plt.figure(figsize=(15, 10))
-        for dataset in DATASETS:
-            # Load data:
-            print("Loading data...")
-            df = pd.read_csv(working_directory + "/sources/" + dataset, index_col=0)
-            df.category = df.category.apply(lambda x: x.replace(" >", ""))
-            df.category = df.category.astype("category")
 
-            accuracies = []
-            for max_len in max_lengths:
-                df_current = df.copy()
+        # Load data:
+        print("Loading data...")
+        df = pd.read_csv(working_directory + "/sources/" + DATASET, index_col=0)
+        df.category = df.category.apply(lambda x: x.replace(" >", ""))
+        df.category = df.category.astype("category")
 
-                print("Transforming data...")
-                input_ids, attention_masks, labels, num_categories, unique_categories = tokenize_data(
-                    df_current.category.values, df_current.category,
-                    max_len=None, tokenizer=transformer
-                )
-                category_dataset = TensorDataset(input_ids, attention_masks, labels)
+        accuracies = []
+        for max_len in max_lengths:
+            print("Transforming data...")
+            # Create a dataset of category embeddings (see dataloader):
+            input_ids, attention_masks, labels, num_categories, unique_categories = tokenize_data(
+                df.category.values, df.category,
+                max_len=None, tokenizer=transformer
+            )
+            category_dataset = TensorDataset(input_ids, attention_masks, labels)
 
-                input_ids, attention_masks, labels, num_categories, unique_categories = tokenize_data(
-                    df_current.description.values, df_current.category,
-                    max_len=max_len, tokenizer=transformer
-                )
-                description_dataset = TensorDataset(input_ids, attention_masks, labels)
+            # Create a dataset of desctiption/sample embeddings (see dataloader):
+            input_ids, attention_masks, labels, num_categories, unique_categories = tokenize_data(
+                df.description.values, df.category,
+                max_len=max_len, tokenizer=transformer
+            )
+            description_dataset = TensorDataset(input_ids, attention_masks, labels)
 
-                train_cat_loader = load_data(category_dataset, BATCH_SIZE, val=False)
-                test_description_loader = load_data(description_dataset, BATCH_SIZE, val=False)
+            # Use pythorch dataloaders:
+            train_cat_loader = load_data(category_dataset, BATCH_SIZE, val=False)
+            test_description_loader = load_data(description_dataset, BATCH_SIZE, val=False)
 
-                # Download model from huggingface.co:
-                model = AutoModel.from_pretrained(
-                    transformer,
-                    num_labels=num_categories,
-                    output_attentions=False,
-                    output_hidden_states=False
-                )
-                model.to(DEVICE)
+            # Download model from huggingface.co:
+            model = AutoModel.from_pretrained(
+                transformer,
+                num_labels=num_categories,
+                output_attentions=False,
+                output_hidden_states=False
+            )
+            model.to(DEVICE)
 
-                cat_embeddings = train(model, train_cat_loader)
-                description_embeddings = train(model, test_description_loader)
+            # Compute embedding vectors:
+            cat_embeddings = train(model, train_cat_loader)
+            description_embeddings = train(model, test_description_loader)
 
-                # For Funnel and BART:
-                if mean:
-                    cat_embeddings = cat_embeddings.mean(dim=1).squeeze()
-                    description_embeddings = description_embeddings.mean(dim=1).squeeze()
+            # For Funnel and BART - merge output embeddings with the mean:
+            if mean:
+                cat_embeddings = cat_embeddings.mean(dim=1).squeeze()
+                description_embeddings = description_embeddings.mean(dim=1).squeeze()
 
-                predictions, predicted_categories, accuracy = evaluate(
-                    cat_embeddings, df_current.category.cat.codes.unique().tolist(),
-                    description_embeddings, description_dataset.tensors[2]
-                )
+            # Evaluate:
+            predictions, predicted_categories, accuracy = evaluate(
+                cat_embeddings, df.category.cat.codes.unique().tolist(),
+                description_embeddings, description_dataset.tensors[2]
+            )
 
-                accuracies.append(accuracy)
+            accuracies.append(accuracy)
 
-                # Output results:
-                print()
-                print(f"Predicted category indices: {predicted_categories}")
-                print(f"Accuracy: {accuracy:.5f}")
+            # Output results:
+            print(f"\nPredicted category indices: {predicted_categories}")
+            print(f"Accuracy: {accuracy:.5f}")
 
-                df_current["predictions"] = [unique_categories[index] for index in predictions.numpy()]
-                print(df_current)
-
-            # Plot results:
-            plt.plot(max_lengths[:-1] + [60], accuracies)
-        plt.xlim(5, 60)
-        plt.xticks(ticks=max_lengths[:-1] + [60], labels=max_lengths[:-1] + ["None"])
-        plt.xlabel("Sequence length", fontsize=15)
-        plt.ylabel("Accuracy", fontsize=15)
-        plt.legend([ds.split(".")[0] for ds in DATASETS])
-        plt.title(f"{transformer_name}'s similarity task (v0) performance", fontsize=20)
-        if os.path.exists(working_directory + f"/images"):
-            plt.savefig(working_directory + f"/images/{transformer_name}_similarity_v0.png", dpi=300)
-        plt.show()
+    # Plot results:
+    plt.plot(max_lengths[:-1] + [60], accuracies)
+    plt.xlim(5, 60)
+    plt.xticks(ticks=max_lengths[:-1] + [60], labels=max_lengths[:-1] + ["None"])
+    plt.xlabel("Sequence length", fontsize=15)
+    plt.ylabel("Accuracy", fontsize=15)
+    plt.legend(list(TRANSFORMERS.keys()))
+    plt.title(f"Similarity task (v0) performance", fontsize=20)
+    if os.path.exists(working_directory + f"/images"):
+        plt.savefig(working_directory + f"/images/similarity_v0.png", dpi=300)
+    # plt.show()
--- models/test_sequence_classification_v1.py
+++ models/test_sequence_classification_v1.py
@@ -8,6 +8,12 @@ from time import time
 
 
 def train(model, train_loader, scheduler, optimizer, loss_function, checkpoint=1):
+    """
+    Fine-tune the given transformer with training sequences
+
+    TODO: add cross-validation with weighted accuracy and the loss (cross-entropy)
+    """
+
     model.train()
 
     avg_loss = 0
@@ -45,6 +51,10 @@ def train(model, train_loader, scheduler, optimizer, loss_function, checkpoint=1
 
 
 def evaluate(model, test_loader):
+    """
+    Evaluate performance on test data
+    """
+
     model.eval()
 
     avg_loss = 0
@@ -84,19 +94,18 @@ NUMBER_OF_SAMPLES = 100
 # DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 DEVICE = torch.device("cpu")
 
-# Transformers to test:
+# Transformers to test
+# second tuple element defines, how many initial blocks of transformer to freeze:
 TRANSFORMERS = {
     "BERT": ('bert-base-uncased', 2),
     "MobileBERT": ('google/mobilebert-uncased', 2),
-    # "FunnelTransformer": ('funnel-transformer/small-base', 1),
-    # "RoBERTa": ('roberta-base', 1),
-    # "BART": ('facebook/bart-base', 1) # bulky
+    "FunnelTransformer": ('funnel-transformer/small-base', 1),
+    "RoBERTa": ('roberta-base', 1),
+    "BART": ('facebook/bart-base', 1) # bulky
 }
 
 # Datasets to test:
-DATASETS = [
-    "data_en_train_with_parent.csv"
-]
+DATASET = "data_en_train_with_parent.csv"
 
 # Hyperparameters:
 EPOCHS = 5
@@ -114,83 +123,81 @@ if __name__ == "__main__":
     
     working_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-    # Load and transform data:
-    DATA_PATH = working_directory + "/sources/data_en_train_with_parent.csv"
-
-    # names = ['sentence_source', 'label', 'label_notes', 'sentence']
-    # df = pd.read_csv(PATH, delimiter="\t", header=None, names=names)[:NUMBER_OF_SAMPLES]
-
+    # Sequence lenghts to vary (careful with the long sequences):
     max_lengths = list(range(25, 60, 10)) + [None]
 
     # Make tests, plot results to compare:
     for transformer_name, (transformer, last_child) in TRANSFORMERS.items():
         plt.figure(figsize=(15, 10))
-        for dataset in DATASETS:
-            df = pd.read_csv(DATA_PATH, index_col=0)
-            print(df)
+        
+        df = pd.read_csv(DATASET, index_col=0)
 
-            accuracies = []
-            for max_len in max_lengths:
-                df_current = df.copy()
+        accuracies = []
+        for max_len in max_lengths:
+            print("Loading data...")
+            input_ids, attention_masks, labels, num_categories, unique_categories = tokenize_data(
+                df.description.values, df.category.astype("category"),
+                max_len=max_len, tokenizer=transformer
+            )
 
-                print("Loading data...")
-                input_ids, attention_masks, labels, num_categories, unique_categories = tokenize_data(
-                    df_current.description.values, df_current.category.astype("category"),
-                    max_len=max_len, tokenizer=transformer
-                )
+            # For instance, validation and training datasets are split manually,
+            # but for Cross-Validation it must be done automatically:
+            # TODO
+            val_dataset = TensorDataset(input_ids[:11], attention_masks[:11], labels[:11])
+            train_dataset = TensorDataset(input_ids[11:], attention_masks[11:], labels[11:])
 
-                val_dataset = TensorDataset(input_ids[:11], attention_masks[:11], labels[:11])
-                train_dataset = TensorDataset(input_ids[11:], attention_masks[11:], labels[11:])
+            train_loader = load_data(train_dataset, BATCH_SIZE, val=False)
+            validation_loader = load_data(val_dataset, BATCH_SIZE, val=False)
 
-                train_loader = load_data(train_dataset, BATCH_SIZE, val=False)
-                validation_loader = load_data(val_dataset, BATCH_SIZE, val=False)
+            # Download model from huggingface.co:
+            model = AutoModelForSequenceClassification.from_pretrained(
+                transformer,
+                num_labels=num_categories,
+                output_attentions=False,
+                output_hidden_states=False
+            )
+            model.to(DEVICE)
 
-                # Download model from huggingface.co:
-                model = AutoModelForSequenceClassification.from_pretrained(
-                    transformer,
-                    num_labels=num_categories,
-                    output_attentions=False,
-                    output_hidden_states=False
-                )
-                model.to(DEVICE)
+            # Freeze the entire model except the last layer:
+            # We can either re-train the whole transformer
+            # or only the last classification layer:
+            for idx, child in enumerate(model.children()):
+                print(f"Child: {idx}")
 
-                # Freeze the entire model except the last layer:
-                # for idx, child in enumerate(model.children()):
-                #     print(f"Child: {idx}")
+                if idx < last_child:
+                    for param in child.parameters():
+                        param.requires_grad = False
 
-                #     if idx < last_child:
-                #         for param in child.parameters():
-                #             param.requires_grad = False
+            # Define optimizer, loss and scheduler:
+            loss_function = CrossEntropyLoss()
+            optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
+            scheduler = get_linear_schedule_with_warmup(
+                optimizer,
+                num_warmup_steps=0,
+                num_training_steps=EPOCHS * len(train_loader)
+            )
 
-                # Define optimizer, loss and scheduler:
-                loss_function = CrossEntropyLoss()
-                optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
-                scheduler = get_linear_schedule_with_warmup(
-                    optimizer,
-                    num_warmup_steps=0,
-                    num_training_steps=EPOCHS * len(train_loader)
-                )
+            # train:
+            print("\nTraining:")
+            start = time()
+            for epoch in range(1, EPOCHS + 1):
+                avg_loss = train(model, train_loader, scheduler, optimizer, loss_function)
+                print(f"Epoch {epoch}: CrossEntropy: {avg_loss}\n")
+            print(time() - start)
+            
+            # evaluate:
+            avg_loss, avg_accuracy, predictions = evaluate(model, validation_loader)
 
-                # train:
-                print("\nTraining:")
-                start = time()
-                for epoch in range(1, EPOCHS + 1):
-                    avg_loss = train(model, train_loader, scheduler, optimizer, loss_function)
-                    print(f"Epoch {epoch}: CrossEntropy: {avg_loss}\n")
-                print(time() - start)
-                
-                # evaluate:
-                avg_loss, avg_accuracy, predictions = evaluate(model, validation_loader)
+            print("\nValidation:\n"
+                f"Val CrossEntropy: {avg_loss:.5f}; Val accuracy: {avg_accuracy:.4f}")
 
-                print("\nValidation:\n"
-                    f"Val CrossEntropy: {avg_loss:.5f}; Val accuracy: {avg_accuracy:.4f}")
+            predicted_labels = [unique_categories[idx] for idx in predictions]
+            print(predictions)
 
-                predicted_labels = [unique_categories[idx] for idx in predictions]
-                print(predictions)
+            accuracies.append(avg_accuracy)
 
-                accuracies.append(avg_accuracy)
-            # Plot results:
-            plt.plot(max_lengths[:-1] + [65], accuracies)
+    # Plot results:
+    plt.plot(max_lengths[:-1] + [65], accuracies)
     plt.xlim(25, 65)
     plt.xticks(ticks=max_lengths[:-1] + [65], labels=max_lengths[:-1] + ["None"])
     plt.xlabel("Sequence length", fontsize=15)
@@ -199,4 +206,4 @@ if __name__ == "__main__":
     plt.title(f"Fine-tuning task (v2) performance", fontsize=20)
     if os.path.exists(working_directory + f"/images"):
         plt.savefig(working_directory + f"/images/fine-tuning_v2.png", dpi=300)
-        # plt.show()
+    # plt.show()
