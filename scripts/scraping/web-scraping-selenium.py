263c535d18193edf1c62168a35f077af3095fe8b
EugeneSel <youdjin.sel15@gmail.com>
2021-03-05 11:53:13 +0100

    Refactored selenium scraper to handle more data

--- scripts/scraping/web-scraping-selenium.py
+++ scripts/scraping/web-scraping-selenium.py
@@ -7,7 +7,7 @@ import json
 import numpy as np
 import pandas as pd
 from googlesearch import search
-from IPython import display
+from IPython.display import display
 
 # TODO: Make Refactoring
 
@@ -22,14 +22,23 @@ working_directory = path.dirname(path.dirname(path.dirname(path.realpath(__file_
 # https://chromedriver.chromium.org/downloads
 DRIVER_PATH = working_directory + '/chromedriver'
 urls_scraping = working_directory + '/sources/input-scrapping.json'
-input_csv_path = working_directory + '/sources/input_scraping_en_train_with_parent.csv'
+input_csv_path = working_directory + '/sources/input_scraping_train_with_parent.csv'
+output_csv_path = working_directory + '/sources/data_train_with_parent.csv'
 
 sites_show_browser = [
     'amazon',
     'fnac',
     'darty',
-    'rakuten'
+    'rakuten',
+    'store.acer',
+    'dell'
 ]
+
+# When to save intermideate results:
+CHECKPOINT = 20
+PREVIOUS_CHECKPOINT = 0
+# Time delay:
+TIME_DELAY = 1
 
 # Url of the page we want to scrape:
 # with open(urls_scraping) as f:
@@ -41,49 +50,59 @@ if __name__ == "__main__":
 
     # Load an input csv:
     df = pd.read_csv(input_csv_path, header=0, index_col=0)
+    df.drop_duplicates(inplace=True, ignore_index=True)
     display(df)
 
     # The dataframe to store scraped data:
     df_data = df.copy()
     df_data.rename(columns={"URL": "description"}, inplace=True)
 
-    for index, row in df.iterrows():
-        print('\nRetrieving: '+ row.URL)
+    if PREVIOUS_CHECKPOINT:
+        # Previous results:
+        df_prev_output = pd.read_csv(output_csv_path, header=0, index_col=0)
+        df_data[:PREVIOUS_CHECKPOINT] = df_prev_output[:PREVIOUS_CHECKPOINT]
+        display(df_data)
 
-        show_browser = not bool([site for site in sites_show_browser if(site in row.URL)])
+    for index, row in df[PREVIOUS_CHECKPOINT:].iterrows():
+        print(f'\n{index} Retrieving: '+ row.URL)
+
+        show_browser = not bool([site for site in sites_show_browser if site in row.URL])
         wait = 0
         if show_browser:
             options.headless = True
             wait = 10
         else:
-            options.headless = False
-            
+            options.headless = True
+            wait = 3
 
-        driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-        driver.implicitly_wait(wait)
-        driver.get(row.URL)
-        # Now, we could simply apply bs4 to request variable
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-        driver.quit()
-
-        time.sleep(1)
-        
-        """Scrape page description."""
         description = None
-        if soup.find("meta", {"name": "description"}):
-            description = soup.find("meta", {"name": "description"}).get('content', None)
-        elif soup.find("meta", {"name": "og:description"}):
-            description = soup.find("meta", {"name": "og:description"}).get('content', None)
-        elif soup.find("meta", {"name": "twitter:description"}):
-            description = soup.find("meta", {"name": "twitter:description"}).get('content', None)
-        elif soup.find("meta", {"property": "description"}):
-            description = soup.find("meta", {"property": "description"}).get('content', None)
-        elif soup.find("meta", {"property": "og:description"}):
-            description = soup.find("meta", {"property": "og:description"}).get('content', None)
-        elif soup.find("meta", {"property": "twitter:description"}):
-            description = soup.find("meta", {"property": "twitter:description"}).get('content', None)
-        
+        try:
+            driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+            driver.implicitly_wait(wait)
+            driver.get(row.URL)
+            # Now, we could simply apply bs4 to request variable
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            driver.quit()
+
+            time.sleep(TIME_DELAY)
+            
+            """Scrape page description."""
+            if soup.find("meta", {"name": "description"}):
+                description = soup.find("meta", {"name": "description"}).get('content', None)
+            elif soup.find("meta", {"name": "og:description"}):
+                description = soup.find("meta", {"name": "og:description"}).get('content', None)
+            elif soup.find("meta", {"name": "twitter:description"}):
+                description = soup.find("meta", {"name": "twitter:description"}).get('content', None)
+            elif soup.find("meta", {"property": "description"}):
+                description = soup.find("meta", {"property": "description"}).get('content', None)
+            elif soup.find("meta", {"property": "og:description"}):
+                description = soup.find("meta", {"property": "og:description"}).get('content', None)
+            elif soup.find("meta", {"property": "twitter:description"}):
+                description = soup.find("meta", {"property": "twitter:description"}).get('content', None)
+        except:
+            print("Connection problem")
+
         if description:
             end_data.append('\n'.join([row.URL, description, row.category]))
             df_data.loc[index, 'description'] = description # add description
@@ -92,10 +111,15 @@ if __name__ == "__main__":
             df_data.loc[index, 'description'] = None # leave an empty case
             print('Description not found')
 
-    # Save collected data in csv format:
-    df_data.to_csv(working_directory + "/sources/data_books_test.csv")
+        # Checkpoint save:
+        if not (index + 1) % CHECKPOINT:
+            print("Checkpoint save...")
+            df_data.iloc[:index + 1, :].to_csv(working_directory + "/sources/data_train_with_parent.csv")
 
-    #Write file
-    with open(working_directory + '/sources/data_books_test.txt', 'w') as f:
+    # Save collected data in csv format:
+    df_data.to_csv(working_directory + "/sources/data_train_with_parent.csv")
+
+    # Write file:
+    with open(working_directory + '/sources/data_train_with_parent.txt', 'w') as f:
         for d in end_data:
             f.write("%s\n\n" % d)
